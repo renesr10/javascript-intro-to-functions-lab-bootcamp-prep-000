b7205228c6ebe8871608bfcf9cd121e1920e7da7
Rene Santana <renesantana10@gmail.com>
2018-06-29 06:02:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,16 +10,15 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 } 
-function sayHiToGrandma(string) {
- if (console.log("I can't hear you!"))
- return string
-} 
-function {
-  if (console.log("YES INDEED!"))
-  return string
+function sayHiToGrandma (string) {
+  if (string.toLowerCase() === string)
+    return "I can't hear you"
+  
+  if (string.toUpperCase() === string)
+    return "YES INDEED!"
+  
+  if (string === "I love you, Grandma")
+  return "I love you, too"
 }
-function sayHiToGrandma(string) {
-  if (string === "I love you, Grandma.")
-  return "I love you, too."
-}
+
 
