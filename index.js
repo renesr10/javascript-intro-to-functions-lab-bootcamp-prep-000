3389c608018ae8e82439e160bade387dc60fb421
Rene Santana <renesantana10@gmail.com>
2018-06-29 05:42:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
-  return string
+  return "I can't hear you!"
 } 
 function sayHiToGrandma(string) {
   if (string.toUpperCase() === string)
-  return string
+  return "YES INDEED!"
 }
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.")
